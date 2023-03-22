d3067842590fde66fa3fd8b5cb69ddf1bce9eade
Sachin Kumar <singhsachin8198@gmail.com>
2023-03-22 15:50:01 +0530

    cards content updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
 import './App.css';
 import TrendingAssetCart from './components/TrendingAssetCart';
 
+const data = [
+  {
+    icon:"/assets/bitcoin.svg", title:"Bitcoin(BTC)", current:31812.80, returns:10, id:1
+  },
+  {
+    icon:"/assets/solana.svg", title:"Solana(SOL)", current:32.83, returns:-12.32, id:2
+  },
+  {
+    icon:"/assets/ethereum.svg", title:"Ethereum(ETH)", current:1466.45, returns:-11.93, id:3
+  },
+  {
+    icon:"/assets/bitcoinsv.svg", title:"Binance USD(BUSD)", current:1.00, returns:0.26, id:4
+  },
+
+  {
+    icon:"/assets/shib.svg", title:"Shiba Inu(SHIB)", current:0.001948, returns:-8.1, id:5
+  }
+
+]
+
 function App() {
   return (
-    <div className=" min-h-screen flex flex-col md:flex-row justify-center items-center bg-[#14172B] md:space-x-9 md:px-10">
-      <TrendingAssetCart/>
-      <TrendingAssetCart/>
-      <TrendingAssetCart/>
-      <TrendingAssetCart/>
-      <TrendingAssetCart/>
+    <div className=" min-h-screen flex flex-col md:flex-row justify-center items-center bg-[#14172B] md:space-x-9 md:px-10 gap-16 sm:gap-0 py-16 ">
+      {
+        data.length > 0 && data?.map((item, index)=><TrendingAssetCart key={item.id} icon={item.icon} title={item.title} current={item.current} returns={item.returns} />)
+      }
     </div>
   );
 }
--- src/components/TrendingAssetCart.tsx
+++ src/components/TrendingAssetCart.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
 
-function TrendingAssetCart() {
+type TrendingProps = {
+  icon: string,
+  title: string,
+  current:number,
+  returns: number
+}
+
+function TrendingAssetCart(props:TrendingProps) {
   return (
     <div className='relative flex items-center justify-center'>
-        <img src="/assets/bitcoin.svg" alt="bitcoin" className=' absolute -top-10 w-[35%]' />
+        <img draggable="false" src={props.icon} alt="bitcoin" className=' absolute -top-9 sm:-top-10 w-[31%] sm:w-[35%]' />
       <img draggable="false" src="/assets/curvybox.svg" alt="boximage"  />
-      <div className='absolute top-12  w-full flex items-center justify-center flex-col '>
-        <h1 className=' font-semibold text-xs text-[#737BAE] leading-8 opacity-90 '>Bitcoin(BTC)</h1>
+      <div className='absolute top-20 sm:top-12  w-full flex items-center justify-center flex-col '>
+        <h1 className=' font-semibold text-xs text-[#737BAE] leading-8 opacity-90 '>{props.title}</h1>
         <div className='bg-[#14172B] rounded-[17px] text-[#ECF0FF] opacity-90 text-base font-semibold leading-8 py-1 w-[90%] text-center'>
-          $31,812.80 <span className='text-[#00FFA3] opacity-90 text-xs ml-3 '>+10%</span>
+          ${props.current} <span className={`${props.returns> 0 ?"text-[#00FFA3]":'text-red-600'} opacity-90 text-xs ml-3`}>{props.returns > 0 ? `+${props.returns}`: props.returns}%</span>
         </div>
         <h1 className='text-[#5A5F7D] opacity-90 text-xs font-semibold leading-8'>Price</h1>
 
@@ -22,9 +28,7 @@ function TrendingAssetCart() {
           <img draggable="false" src="/assets/bnb.svg" alt="icon" />
         </div>
       <h1 className=' text-[#5A5F7D] opacity-90 leading-8 font-semibold text-xs'>Popular pairs</h1>
-
       </div>
-
       </div>
   )
 }
